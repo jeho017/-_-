7d340d42205f999e820805108b20a0601b3e48ed
jeho017 <leejeho017@gmail.com>
2024-07-30 13:03:23 +0900

    세번째 실습

---
+++ 240730/solution2.js
@@ -0,0 +1,12 @@
+let tutorNames = ["최원장", "김르탄", "윤창식", "박가현", "김병연", "내배캠"];
+
+tutorNames.forEach(function (name) {
+  if (
+    name === "최원장" ||
+    name === "윤창식" ||
+    name === "박가현" ||
+    name === "김병연"
+  ) {
+    console.log(name);
+  }
+});
